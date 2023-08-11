766fe5d0deb335f5b3f030b01c7fe2632ac080cd
Ximena Elgart <ximenaelgart.dev@gmail.com>
2023-08-11 11:04:40 -0300

    tg-58 Create epic model based on the schema

---
+++ src/app/modules/models/epic.model.ts
@@ -0,0 +1,29 @@
+import { Item } from "./item.model";
+import { Project } from "./project.model";
+
+export class Epic extends Item {
+    private _project: Project;
+    private _icon: string | undefined;
+
+    constructor(name: string, description: string, project: Project, icon?: string) {
+        super(name, description);
+        this._project = project;
+        this._icon = icon;
+    }
+    
+    get project(): Project {
+        return this._project;
+    }
+
+    get icon(): string | undefined {
+        return this._icon;
+    }
+
+    set project(project: Project) {
+        this._project = project;
+    }
+
+    set icon(icon: string) {
+        this._icon = icon;
+    }
+}
