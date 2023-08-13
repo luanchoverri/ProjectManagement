621c2fcf255b727d6612a27f98a19290b905a514
Ximena Elgart <ximenaelgart.dev@gmail.com>
2023-08-13 17:56:05 -0300

    tg-62 Add route to epic list view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { HomeComponent } from './modules/presentation/views/home/home.component'
 import { MyProjectsComponent } from './modules/presentation/views/my-projects/my-projects.component';
 import { MyStoriesComponent } from './modules/presentation/views/my-stories/my-stories.component';
 import { SettingsComponent } from './modules/presentation/views/settings/settings.component';
+import { MyEpicsComponent } from './modules/presentation/views/my-epics/my-epics.component';
 
 const routes: Routes = [
   {path: 'home', component:HomeComponent},
-  {path: 'my-projects', component:MyProjectsComponent},
+  {path: 'my-projects', component:MyProjectsComponent, children: [
+    { path: ':projectId', component: MyEpicsComponent }
+  ]},
   {path: 'my-stories', component:MyStoriesComponent},
   {path: 'settings', component:SettingsComponent}
 ];
