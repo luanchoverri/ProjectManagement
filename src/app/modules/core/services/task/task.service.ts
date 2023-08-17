a71048ad0e16f5c1f4807732e7bab3a2fe1ca9ba
Ximena Elgart <ximenaelgart.dev@gmail.com>
2023-08-17 19:32:30 -0300

    tg-77 Create task service

---
+++ src/app/modules/core/services/task/task.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Task } from 'src/app/modules/models/task.model';
+import { LocalStorageService } from '../localStorage/local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private readonly TASK_KEY = 'tasks';
+  taskList$ = new Observable<Task[]>();
+
+  //datos mock
+  taskList: Task[] = [
+    new Task("Implement Translation Algorithm", "Code the translation algorithm.", 11),
+    new Task("UI Integration", "Integrate the translation module with the user interface.", 11)
+  ]
+
+  constructor(private ls: LocalStorageService) {
+    this.ls.updateItem(this.TASK_KEY, this.taskList);
+  }
+
+  getItems(): Observable<Task[]> {
+    return this.ls.getItem<Task[]>(this.TASK_KEY).pipe(map((data) => data || []));
+  }
+
+
+  getItemById(id: number): Task | undefined{
+    return this.taskList.find((item) => item.id == id);
+  }
+
+
+  getTasksByStoryId(id : number): Task[] {
+    return this.taskList.filter(task => task.story === id);
+  }
+}
