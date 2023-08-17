300fcbd66f404145d014b612447bdbcbf405ccdb
Ximena Elgart <ximenaelgart.dev@gmail.com>
2023-08-17 19:09:22 -0300

    tg-76 Create task model based on the schema

---
+++ src/app/modules/models/task.model.ts
@@ -0,0 +1,60 @@
+import { Type } from "./enum";
+import { Item } from "./item.model";
+import { Story } from "./story";
+
+export class Task extends Item {
+
+    // private _story: Story; 
+    private _story: number;
+    private _created: Date;
+    private _dueDate?: Date;
+    private _done: boolean;
+    private static Type: Type;
+
+    constructor(name: string, description: string, story: number, created?: Date, dueDate?: Date, done?: boolean) {
+        super(name, description);
+        this._story = story;
+        this._created = created || new Date();
+        this._dueDate = dueDate;
+        this._done = done || false;
+    }
+    
+    get type(): Type {
+        return Type.Task;
+    }
+
+    // Getters
+    get story(): number {
+        return this._story;
+    }
+
+    get created(): Date | undefined {
+        return this._created;
+    }
+
+    get dueDate(): Date | undefined {
+        return this._dueDate;
+    }
+
+    get done(): boolean | undefined {
+        return this._done;
+    }
+
+    // Setters
+    set story(value: number) {
+        this._story = value;
+    }
+
+    set created(value: Date) {
+        this._created = value;
+    }
+
+    set dueDate(value: Date) {
+        this._dueDate = value;
+    }
+
+    set done(value: boolean) {
+        this._done = value;
+    }
+
+}
