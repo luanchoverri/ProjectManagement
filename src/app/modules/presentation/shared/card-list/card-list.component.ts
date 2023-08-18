93fc89e5b72dcafceb709b9de8ac8185645abb16
nencinosa <ninaencinosacastro@gmail.com>
2023-08-18 11:15:44 -0300

    tg-84 Fix card-list & item-card routing link

--- src/app/modules/presentation/shared/card-list/card-list.component.ts
+++ src/app/modules/presentation/shared/card-list/card-list.component.ts
@@ -1,43 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ListService } from 'src/app/modules/core/services/list/list.service';
-import { Type } from 'src/app/modules/models/enum';
-import { Epic } from 'src/app/modules/models/epic.model';
+import { Component, Input } from '@angular/core';
 import { Item } from 'src/app/modules/models/item.model';
-import { Project } from 'src/app/modules/models/project.model';
-import { Story } from 'src/app/modules/models/story';
+
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.scss'],
 })
-export class CardListComponent implements OnInit {
+export class CardListComponent {
   @Input() list: Item[] | undefined;
 
-  linkSize: number = 0;
-  routerLink: string = '/';
-  currentLink!: string;
+  constructor() {}
 
-  constructor(
-    private route: ActivatedRoute,
-  ) {
-    this.currentLink = '/';
-  }
-
-  ngOnInit(): void {}
-
-  getCurrentRoute(item: Item): string {
-    this.linkSize = this.route.snapshot.url.length;
-
-    for (let i = 0; i < this.linkSize; i++) {
-      this.currentLink += this.route.snapshot.url[i].path + '/';
-    }
-
-    this.currentLink += `${item._id}`;
-
-    this.routerLink = this.currentLink;
-    this.currentLink = '/';
-    return this.routerLink;
-  }
 }
--- src/app/modules/presentation/shared/item-card/item-card.component.ts
+++ src/app/modules/presentation/shared/item-card/item-card.component.ts
@@ -9,10 +9,36 @@ import { Item } from 'src/app/modules/models/item.model';
 })
 export class ItemCardComponent {
   @Input() item !: Item;
+  linkSize: number = 0;
+  routerLink: string = '/';
+  currentLink!: string;
  
+  constructor(private route: ActivatedRoute) {
+      this.currentLink = '/';
+  }
 
-  ngOnInit(): void {
+  ngOnInit( ): void {
     
+  }
+
+  getCurrentRoute(item: Item): string {
+    this.linkSize = this.route.snapshot.url.length;
+
+    for (let i = 0; i < this.linkSize; i++) {
+      this.currentLink += this.route.snapshot.url[i].path + '/';
+    }
+    this.currentLink += `${item._id}`;
+    this.routerLink = this.currentLink;
+    this.currentLink = '/';
+    return this.routerLink;
+  }
+
+  editItem() {
+    console.log("editting... "+this.item);
+  }
+
+  deleteItem() {
+    console.log("deleting... "+this.item);
   }
 
 }
