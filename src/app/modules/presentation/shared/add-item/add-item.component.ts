66ead216bbea55937f8b49fed212b0f04f6b6096
nencinosa <ninaencinosacastro@gmail.com>
2023-08-18 21:30:17 -0300

    tg-83 Create component add-item

---
+++ src/app/modules/presentation/shared/add-item/add-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-item',
+  templateUrl: './add-item.component.html',
+  styleUrls: ['./add-item.component.scss'],
+})
+export class AddItemComponent {
+  @Input() formDialog: any;
+
+  constructor(public dialog: MatDialog) {
+    this.formDialog = '';
+  }
+
+  openDialog() {
+    this.dialog.open(this.formDialog,{
+      width: '30vw'
+    });
+  }
+}
