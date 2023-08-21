41cad4005a394ae02b4b34b5d2e9c161030287c8
Ximena Elgart <ximenaelgart.dev@gmail.com>
2023-08-21 08:41:34 -0300

    tg-111 Delete task

--- src/app/modules/core/services/task/task.service.ts
+++ src/app/modules/core/services/task/task.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { Task } from 'src/app/modules/models/task.model';
 import { LocalStorageService } from '../localStorage/local-storage.service';
 import { ListService } from '../list/list.service';
 import { HttpClient } from '@angular/common/http';
 import { ApiResponse } from 'src/app/modules/models/apiResponse';
 import { PathRest } from 'src/app/modules/api-rest/enviroments/path-rest';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,11 @@ export class TaskService extends ListService<Task> {
   // new Task("UI Integration", "Integrate the translation module with the user interface.", '11')
   // ]
 
-  constructor(private ls: LocalStorageService, private http: HttpClient) {
+  constructor(
+    private ls: LocalStorageService, 
+    private http: HttpClient,
+    private snackBar: MatSnackBar
+  ) {
     // this.ls.updateItem(this.TASK_KEY, this.taskList);
     super();
   }
@@ -39,8 +44,20 @@ export class TaskService extends ListService<Task> {
     console.log("voy a actualizar el task");
     return new Observable<Task>;
   }
-  override deleteItem(id: string): Observable<Task> {
-    console.log("voy a borrar el task");
-    return new Observable<Task>;
+  
+  override deleteItem(id: string): Observable<Task | null> {
+    return this.http
+      .delete<ApiResponse>(`${PathRest.GET_PROJECTS}/${id}`)
+      .pipe(
+        map((response) => response.data),
+        catchError(error => {
+          return of(null);
+        }),
+        tap(() => {
+          this.snackBar.open('Epic deleted successfully.', 'Close', {
+            duration: 5000,
+          });
+        })
+      );
   }
 }
