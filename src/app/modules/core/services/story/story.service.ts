11e45fc0e5adb00563171d538c5fd75091469d2e
Ximena Elgart <ximenaelgart.dev@gmail.com>
2023-08-21 22:19:24 -0300

    tg-106 Edit story

--- src/app/modules/core/services/story/story.service.ts
+++ src/app/modules/core/services/story/story.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ListService } from '../list/list.service';
 import { Story } from 'src/app/modules/models/story';
-import { State } from 'src/app/modules/models/enum';
 import { LocalStorageService } from '../localStorage/local-storage.service';
 import { Observable, catchError, map, of, switchMap, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -9,92 +8,23 @@ import { AuthService } from 'src/app/modules/api-rest/services/auth.service';
 import { PathRest } from 'src/app/modules/api-rest/enviroments/path-rest';
 import { ApiResponse } from 'src/app/modules/models/apiResponse';
 import { Task } from 'src/app/modules/models/task.model';
-import { endpoint } from 'src/app/modules/api-rest/enviroments/endpoints';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { StoryFormComponent } from 'src/app/modules/presentation/feature/forms/story-form/story-form.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoryService extends ListService<Story> {
-  //datos mock
-  // storyList: Story[] = [
-  //   new Story(
-  //     "Enhance User Onboarding Process",
-  //     "Revamp the user onboarding process to improve user engagement.",
-  //     8,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     undefined,
-  //     undefined
-  // ),
-  //   new Story(
-  //     'Upgrade Security System',
-  //     'Implement advanced security measures to protect user data.',
-  //     7,
-  //     'Owner',
-  //     ['Alice', 'Bob'],
-  //     8,
-  //     new Date('2023-09-01'),
-  //     new Date('2023-09-15'),
-  //     new Date('2023-09-02'),
-  //     new Date('2023-09-14'),
-  //     State.Running
-  //   ),
-  //   new Story(
-  //     'Develop Personalized Recommendations',
-  //     'Create an algorithm to provide personalized product recommendations to users.',
-  //     7,
-  //     'TechWizard',
-  //     ['Elena', 'Oliver'],
-  //     5,
-  //     new Date('2023-08-15'),
-  //     new Date('2023-08-31'),
-  //     new Date('2023-08-20'),
-  //     new Date('2023-08-30'),
-  //     State.Done
-  //   ),
-  //   new Story(
-  //     'Create Language Translation Module',
-  //     'Develop a module for automatic language translation in the application.',
-  //     6,
-  //     'Owner',
-  //     ['Sophia', 'Lucas'],
-  //     3,
-  //     new Date('2023-07-01'),
-  //     new Date('2023-08-10'),
-  //     new Date('2023-07-05'),
-  //     new Date('2023-08-08'),
-  //     State.Todo
-  //   ),
-  //   new Story(
-  //     'Optimize Inventory Management',
-  //     'Implement an AI-based solution to streamline inventory management.',
-  //     6,
-  //     'InventoryMaster',
-  //     ['Grace', 'David'],
-  //     1,
-  //     new Date('2023-10-10'),
-  //     new Date('2023-11-15'),
-  //     new Date('2023-10-12'),
-  //     new Date('2023-11-10'),
-  //     State.Todo
-  //   ),
-  // ];
-
   storiesList$ = new Observable<Story[]>();
 
   constructor(
     private ls: LocalStorageService,
     private http: HttpClient,
     private authService: AuthService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
   ) {
-    // //carga de datos mock
-    // this.ls.updateItem('stories', this.storyList);
     super();
   }
 
@@ -104,28 +34,45 @@ export class StoryService extends ListService<Story> {
 
   override createItem(item: Story): Observable<Story> {
     return this.http
-    .post<ApiResponse>(PathRest.GET_STORIES, item)
-    .pipe(map((response) => response.data));
+      .post<ApiResponse>(PathRest.GET_STORIES, item)
+      .pipe(map((response) => response.data));
   }
 
-  override updateItem(item: Story): Observable<Story> {
-    console.log("voy a actualizar el story");
-    return new Observable<Story>;
+  editItem(story: Story): void {
+    const dialogRef = this.dialog.open(StoryFormComponent, {
+      data: { initialValues: story },
+    });
+
+    dialogRef.componentInstance.toggleIsEditing();
+  }
+
+  override updateItem(item: Story): Observable<Story | null> {
+    return this.http
+      .put<ApiResponse>(`${PathRest.GET_STORIES}/${item._id}`, item)
+      .pipe(
+        map((response) => response.data),
+        catchError(() => of(null))
+      );
   }
 
   override deleteItem(id: string): Observable<Story | null> {
     return this.getTasksByStory(id).pipe(
-      switchMap(tasks => {
+      switchMap((tasks) => {
         if (tasks.length > 0) {
-          this.snackBar.open('This story has associated tasks and cannot be deleted.', 'Close', {
-            duration: 5000,
-          });
-          return of(null); 
+          this.snackBar.open(
+            'This story has associated tasks and cannot be deleted.',
+            'Close',
+            {
+              duration: 5000,
+            }
+          );
+          return of(null);
         } else {
-          return this.http.delete<ApiResponse>(`${PathRest.GET_STORIES}/${id}`)
+          return this.http
+            .delete<ApiResponse>(`${PathRest.GET_STORIES}/${id}`)
             .pipe(
               map((response) => response.data),
-              catchError(error => {
+              catchError((error) => {
                 return of(null);
               }),
               tap(() => {
@@ -139,36 +86,23 @@ export class StoryService extends ListService<Story> {
     );
   }
 
-  // getItemById(id: number): Story | undefined{
-  //   return this.storyList.find((item) => item.id == id);
-  // }
-
-  // getStoriesByEpicId(id : number): Story[] {
-  //   return this.storyList.filter(story => story.epic === id);
-  // }
-
   getStoryById(id: string): Observable<Story> {
-    // if (this.isLoggedIn) {
- 
     return this.http
       .get<ApiResponse>(`${PathRest.GET_STORIES}/${id}`)
       .pipe(map((response) => response.data));
   }
 
   getTasksByStory(id: string): Observable<Task[]> {
-    return this.http
-      .get<ApiResponse>(`${PathRest.GET_TASKS}`)
-      .pipe(
-        map((response) => response.data),
-        catchError(() => of([]))
-      );
+    return this.http.get<ApiResponse>(`${PathRest.GET_TASKS}`).pipe(
+      map((response) => response.data),
+      catchError(() => of([]))
+    );
   }
 
   getAll(): Observable<Story[]> {
-  
     return this.http.get<ApiResponse>(PathRest.GET_STORIES).pipe(
       map((response) => response.data),
-      catchError(() => of([])) 
+      catchError(() => of([]))
     );
   }
 }
--- src/app/modules/presentation/feature/forms/story-form/story-form.component.ts
+++ src/app/modules/presentation/feature/forms/story-form/story-form.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { StoryService } from 'src/app/modules/core/services/story/story.service';
@@ -16,8 +17,15 @@ export class StoryFormComponent {
   myForm!: FormGroup;
   members!: User[];
   members$: Observable<User[]>;
+  isEditing: boolean = false;
 
-  constructor(private fb: FormBuilder, private ss: StoryService,private us: UserService, private router: Router) {
+  constructor(
+    private fb: FormBuilder, 
+    private ss: StoryService,
+    private us: UserService, 
+    private router: Router,
+    @Inject(MAT_DIALOG_DATA) public data: any
+    ) {
     this.epicId = router.url.split('?')[0].split('/').pop()!;
     this.members$ = us.getUsers();
     if (this.members$) {
@@ -28,25 +36,51 @@ export class StoryFormComponent {
   }
 
   ngOnInit() {
-    this.myForm = this.fb.group({
-      name: new FormControl('', Validators.required),
-      description: new FormControl(''),
-      epic: new FormControl(this.epicId),
-      owner: new FormControl(''),
-      assignedTo: new FormControl(''),
-      points : new FormControl(''),
-      created: new FormControl(new Date()),
-      due: new FormControl(''),
-      started: new FormControl(''),
-      finished: new FormControl(''),
-      status: new FormControl(''),
-      icon: new FormControl(''),
-    });
+    if (this.isEditing) {
+      this.myForm = this.fb.group({
+        _id: new FormControl(this.data.initialValues._id),
+        name: new FormControl(this.data.initialValues.name, Validators.required),
+        description: new FormControl(this.data.initialValues.description),
+        epic: new FormControl(this.epicId),
+        owner: new FormControl(this.data.initialValues.owner),
+        assignedTo: new FormControl(this.data.initialValues.assignedTo),
+        points : new FormControl(this.data.initialValues.points),
+        created: new FormControl(this.data.initialValues.created),
+        due: new FormControl(this.data.initialValues.due),
+        started: new FormControl(this.data.initialValues.started),
+        finished: new FormControl(this.data.initialValues.finished),
+        status: new FormControl(this.data.initialValues.status),
+        icon: new FormControl(this.data.initialValues.icon),
+      });
+    } else {
+      this.myForm = this.fb.group({
+        name: new FormControl('', Validators.required),
+        description: new FormControl(''),
+        epic: new FormControl(this.epicId),
+        owner: new FormControl(''),
+        assignedTo: new FormControl(''),
+        points : new FormControl(''),
+        created: new FormControl(new Date()),
+        due: new FormControl(''),
+        started: new FormControl(''),
+        finished: new FormControl(''),
+        status: new FormControl(''),
+        icon: new FormControl(''),
+      });
+    }
   }
 
   onSubmit() {
-    this.ss.createItem(this.myForm.value).subscribe((data) => {
-    });
+    if (this.isEditing) {        
+      this.ss.updateItem(this.myForm.value).subscribe();
+    } else {
+      this.ss.createItem(this.myForm.value).subscribe();
+    }
+
   }
+
+  toggleIsEditing() {
+    this.isEditing = !this.isEditing;
+  }  
 
 }
