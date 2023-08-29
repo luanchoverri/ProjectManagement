8c8cc624b1d97b8ffa16a392354edaa03a77251a
Ximena Elgart <ximenaelgart.dev@gmail.com>
2023-08-29 09:35:47 -0300

    tg-186 Fix breadcrumb in mobile

--- src/app/modules/presentation/core/core.module.ts
+++ src/app/modules/presentation/core/core.module.ts
@@ -7,12 +7,14 @@ import { SharedModule } from '../shared/shared.module';
 import { FeatureModule } from '../feature/feature.module';
 import { RouterModule } from '@angular/router';
 import { BreadcrumbModule } from 'xng-breadcrumb';
+import { BreadcrumbComponent } from './header/breadcrumb/breadcrumb.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     MainComponent,
+    BreadcrumbComponent,
     
   ],
   imports: [
