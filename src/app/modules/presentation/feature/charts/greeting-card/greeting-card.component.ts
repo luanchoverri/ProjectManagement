7d58ccfec67155422afc7ed6a7814890a4306e9f
luanchoverri <lu.anchoverri@gmail.com>
2023-09-01 20:54:25 -0300

    fuera del final commmit: username en la greeting card + fix dona (gracias ninaa x2)

--- src/app/modules/presentation/feature/charts/greeting-card/greeting-card.component.ts
+++ src/app/modules/presentation/feature/charts/greeting-card/greeting-card.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../api-rest/services/auth.service';
+import { User } from 'src/app/modules/models/user';
+import { UserService } from 'src/app/modules/core/services/user/user.service';
 
 @Component({
   selector: 'app-greeting-card',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./greeting-card.component.scss']
 })
 export class GreetingCardComponent {
+  user!: User | null;
+
+
+  
+  constructor(private authService: AuthService, private us: UserService){}
+
+
+  ngOnInit(){
+    const userId = this.authService.getUserId();
+    if (userId) {
+      this.us.getUserById(userId).subscribe(
+        (userOrNull) => {
+          this.user = userOrNull;
+        }
+      );
+    }
+  }
+
 
 }
